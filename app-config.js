6cd54a3d1d94371530ae115f10313b1a9e386e68
muangchang <sudarat.0807@gmail.com>
2026-09-07 11:34:52 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbw7gUWih1UGW37lxpi1jj98G0k35iVN7FVRMP-E4HeMs0ea54oTtAI9f2uj4OYHPx4w/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycby_vD4r4Gz8ShCJ8LzNs_9Iiev6FVJT8EmiMDY9_9-vlpoqJjM43-q9mu0iTztQlfFt/exec'
   });
 })();
