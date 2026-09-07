18ddc5a5fa9470c5c7fcbb738945b08c3a2a0415
muangchang <sudarat.0807@gmail.com>
2026-09-07 11:36:57 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbzhKSlQbgapfkxuCyc8uouU9B4EtgZRKd6uBVSCWG6yKZu-O4Aoi1gX7pRdR8g83fI5/exec';
